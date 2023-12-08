0ed19ed3882a47011cc4abaecf4bdc77562d6233
vladihka <vladihka58@gmail.com>
2023-12-08 15:24:55 +0100

    Adding search bar to the Header component

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,39 @@
 import Image from 'next/image';
 import React from 'react';
+import SearchIcon from "../Icons/SearchIcon";
+
 
 export default function Header() {
-  return <div>
-      {/* Left */}
+      return (
+          <div className="flex items-center justify-between max-w-6xl">
+              <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+                  <Image
+                        src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+                        layout='fill'
+                        className="object-contain"
+                    />
+                  </div>
+                  <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+                      <Image
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+                            layout='fill'
+                            className="object-contain"
+                      />
+                  </div>
 
-      <div className="flex items-center justify-between max-w-6xl">
-          <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-              <Image
-                src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-                layout='fill'
-                className="object-contain"
-              />
-          </div>
-          <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
-              <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
-                layout='fill'
-                className="object-contain"
-              />
-          </div>
+                <div className="relative mt-1">
+                    <div className="absolute top-2 left-2">
+                        <SearchIcon className={"h-5 text-gray-500"}></SearchIcon>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black
+                        rounded-md"
+                    />
+                </div>
 
-          <h1>Right sides</h1>
-      </div>
-
-      {/* Middle */}
-
-      {/* Right */}
-  </div>;
+                <h1>Right sides</h1>
+            </div>
+      );
 }
