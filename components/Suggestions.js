375ebfa9031f7092acbb0b4d6ba08fdca138a9c0
vladihka <vladihka58@gmail.com>
2023-12-08 19:27:18 +0100

    Fixing key

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -23,6 +23,7 @@ export default function Suggestions(){
             {suggestions.map(suggestion => (
                 <div className="flex items-center justify-between mt-3">
                     <img
+                        key={suggestion.id}
                         className="h-10 rounded-full border p-[2px]"
                         src={`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`}
                         alt=""/>
