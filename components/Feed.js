a1c80b6046a193c4ed535f04e8d9ac55087a1fbf
vladihka <vladihka58@gmail.com>
2023-12-08 16:28:43 +0100

    Populate fake data using minifaker

---
+++ components/Feed.js
@@ -0,0 +1,14 @@
+import Stories from "./Stories";
+
+export default function Feed(){
+    return(
+        <main>
+            <section>
+                <Stories></Stories>
+            </section>
+            <section>
+
+            </section>
+        </main>
+    )
+}
