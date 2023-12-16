a0d8a442c7b6556b95e54bb9aeeb7896c55c5a28
vladihka <vladihka58@gmail.com>
2023-12-16 12:26:27 +0100

    Get the post datas from database and show it in the feed section

--- components/Posts.js
+++ components/Posts.js
@@ -1,32 +1,28 @@
 import Post from "./Post";
+import {useEffect, useState} from "react";
+import {collection, onSnapshot, query, orderBy} from "firebase/firestore"
+import {db} from "../firebase";
 
 export default function Posts(){
-    const posts = [
-        {
-            id: "1",
-            username: "vladihka",
-            userImg: "https://media.licdn.com/dms/image/C4E03AQFbkO6E7nvOYQ/profile-displayphoto-shrink_800_800/0/1655032591139?e=2147483647&v=beta&t=vLFbMqVJpf-hgFUevz4UWnlGasQok9gYhI_RpU6KsOg",
-            img: "https://images.unsplash.com/photo-1701435135977-5cad492e91ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8",
-            caption: "Nice picture"
-        },
-        {
-            id: "2",
-            username: "n_alaska",
-            userImg: "https://media.licdn.com/dms/image/C4E03AQFbkO6E7nvOYQ/profile-displayphoto-shrink_800_800/0/1655032591139?e=2147483647&v=beta&t=vLFbMqVJpf-hgFUevz4UWnlGasQok9gYhI_RpU6KsOg",
-            img: "https://images.unsplash.com/photo-1701986789735-6e266acb128f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D",
-            caption: "New picture from my city"
-        },
-    ]
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            query(collection(db, "posts"), orderBy(("timestamp"), "desc")), (snapshot) => {
+                setPosts(snapshot.docs)
+            }
+        )
+        return unsubscribe;
+    }, []);
     return(
         <div>
             {posts.map(post => (
                 <Post
                     key={post.id}
                     id={post.id}
-                    userImg={post.userImg}
-                    img={post.img}
-                    caption={post.caption}
-                    username={post.username}>
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
+                    username={post.data().username}>
                 </Post>
             ))}
         </div>
