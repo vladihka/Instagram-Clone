213ad72eed52c023f35ed5d55f86208a373288cf
vladihka <vladihka58@gmail.com>
2023-12-16 13:13:37 +0100

    Show comments in the comments section from the firebase database

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,23 @@
 import {BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon} from "@heroicons/react/outline";
 import {useSession} from "next-auth/react";
-import {useState} from "react";
-import {addDoc, collection, serverTimestamp} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {addDoc, collection, onSnapshot, orderBy, query, serverTimestamp} from "firebase/firestore";
 import {db} from "../firebase";
+import Moment from 'react-moment';
 
 export default function Post({img, userImg, caption, username, id}){
 
     const {data:session} = useSession();
     const [comment, setComment] = useState("");
+    const [comments, setComments] = useState([]);
+
+    useEffect(() => {
+        const unsibscribe = onSnapshot(
+            query(collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")),
+            (snapshot) => {setComments(snapshot.docs)}
+        )
+
+    }, [db, id]);
 
     async function sentComment(event){
         event.preventDefault();
@@ -45,6 +55,21 @@ export default function Post({img, userImg, caption, username, id}){
             )}
 
             <p className="p-5 truncate"><span className="font-bold mr-2">{username}</span>{caption}</p>
+            {comments.length > 0 && (
+                <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+                    {comments.map(comment => (
+                        <div className="flex items-center space-x-2 mb-2">
+                            <img
+                                className="h-7 rounded-full object-cover"
+                                src={comment.data().userImage}
+                                alt="user-image"/>
+                            <p className="font-semibold">{comment.data().username}</p>
+                            <p className="flex-1 truncate">{comment.data().comment}</p>
+                            <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+                        </div>
+                    ))}
+                </div>
+            )}
 
             {session && (
                 <form action="" className="flex items-center p-4">
